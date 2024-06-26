d3aad26223474811ada8addefd6fc3689f6c88c3
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-06-26 21:09:49 +0530

    refactor: Refactor GlobalStyleProvider

--- app/Providers/GlobalStyleProvider.tsx
+++ app/Providers/GlobalStyleProvider.tsx
@@ -1,13 +1,15 @@
 "use client";
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components';
 
 interface Props {
   children: React.ReactNode;
 }
 
-function GlobalStyleProvider({ children }: Props) {
-  return <GlobalStyles>{children}</GlobalStyles>;
+function GlobalStyleProvider({ children}:Props ) {
+  return (
+    <GlobalStyles>{children}</GlobalStyles>
+  )
 }
 
 const GlobalStyles = styled.div`
@@ -29,4 +31,4 @@ const GlobalStyles = styled.div`
   }
 `;
 
-export default GlobalStyleProvider;
+export default GlobalStyleProvider
